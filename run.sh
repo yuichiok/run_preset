925b0c1465671b6e6f2f73fe7b8240983c9202de
OKUGAWA Yuichi <yokugawa@cw14.cc.kek.jp>
2019-06-28 13:20:35 +0900

    changed location and added new parameters to the job

--- run.sh
+++ run.sh
@@ -1,36 +1,64 @@
 #!/bin/bash
 
 inputxml=preset.xml
-#inputxml=preset_NoNewVtx.xml
 
-###======= 500 GeV =======
+# small or large
+#model=l5
+model=s5
 
-##=====new 6f_ttbar samples
-#6f_ttbar/yyxylv.eL.pR
-#dirin=/hsm/ilc/grid/storm/prod/ilc/mc-dbd/ild/dst/500-TDR_ws/6f_ttbar/ILD_o1_v05/v01-16-p05_500/
-
-##===== DBD sample =====##
-#dirin=/hsm/ilc/grid/storm/prod/ilc/mc-dbd/ild/dst-merged/500-TDR_ws/6f_ttbar/ILD_o1_v05/v01-16-p05_500/
-
-##===== New sample (large detector) =====##
-#dirin=/hsm/ilc/grid/storm/prod/ilc/mc-opt-3/ild/dst-merged/500-TDR_ws/6f_ttbar/ILD_l5_o1_v02/v02-00-01/
-
-##===== New sample (small detector) =====##
-#dirin=/hsm/ilc/grid/storm/prod/ilc/mc-opt-3/ild/dst-merged/500-TDR_ws/6f_ttbar/ILD_s5_o1_v02/v02-00-01/
-
-##===== New sample (large, new vertexing) =====##
-dirin=/hsm/ilc/users/yonamine/physics/lcfiplus/vertexing_6f_ttbar/
-
-#processID=$1
+gearxml=gear_ILD_${model}_v02.xml
 
 # yyxyev
 #processID=108670
 # yyxylv
-processID=108675
+#processID=108675
 
-#nfile=-1
-nfile=12
+# eLpR
+#processID=eL.pR
+#processID=I108670
+
+# type isolated lepton
+isolep=electron
+#isolep=muon
+#isolep=tau
+#isolep=muon_tau
+
+
+##===== New sample (large, new vertexing) =====##
+
+dirin=/hsm/ilc/users/yokugawa/
+processID=default
+
+if [ "$model" == "l5" ]; then
+	if [ "$isolep" == "electron" ]; then
+		dirin=/hsm/ilc/users/yonamine/physics/lcfiplus/vertexing_6f_ttbar/
+		processID=I108668
+		#processID=I108670
+	else
+		dirin=/home/ilc/yonamine/work/Yuichi/test20190328/run/slcio/
+		processID=eL.pR
+	fi
+elif [ "$model" == "s5" ]; then
+	if [ "$isolep" == "electron" ]; then
+		dirin=/hsm/ilc/users/yonamine/physics/lcfiplus/vertexing_6f_ttbar_s5/
+		processID=I108668
+		#processID=I108670
+	else
+		dirin=/home/ilc/yonamine/work/Yuichi/test20190328/run_s5/slcio/
+		processID=eL.pR
+	fi
+fi
+
+
+# electron sample
+#dirin=/hsm/ilc/users/yonamine/physics/lcfiplus/vertexing_6f_ttbar/
+
+# muon and tau sample
+#dirin=/home/ilc/yonamine/work/Yuichi/test20190328/run/slcio/
+
+nfile=-1
+#nfile=12
 #nfile=1
 #nperjob=50
-nperjob=1
-./submit2 $dirin $processID 1 $nfile $nperjob $inputxml
+nperjob=3
+./submit2 $dirin $processID 1 $nfile $nperjob $inputxml $gearxml $isolep $model
